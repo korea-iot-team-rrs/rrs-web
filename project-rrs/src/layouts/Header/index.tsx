359d8d8a0245c4eea0c4f4d96f10f4aece1b2899
kim dahye <makamong0000@gmail.com>
2024-12-19 20:51:38 +0900

    i made folder but it's not finished yet 241219 kdh

--- project-rrs/src/layouts/Header/index.tsx
+++ project-rrs/src/layouts/Header/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default function Header() {
-  return (
-    <div>Header</div>
-  )
+export default function index() {
+  return <>
+      <header>
+        <h1>RRS</h1>
+        <nav>
+          <ul>
+            <li>댕수첩</li>
+            <li>댕시터</li>
+            <li>댕소통</li>
+            <li>고객센터</li>
+          </ul>
+        </nav>
+      </header>
+    </>
 }
