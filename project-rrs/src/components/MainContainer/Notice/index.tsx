9c9aad0de473a5aa645ea316f28b5c7f58e62325
sometime1992 <sometime1992@naver.com>
2024-12-26 17:21:10 +0900

    20241226 cjh

--- project-rrs/src/components/MainContainer/Notice/index.tsx
+++ project-rrs/src/components/MainContainer/Notice/index.tsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Notice() {
-    return (
-        <div className="box noticeAndPetsitter">
-          <div className="notice">
-            <ul>
-              <h2>Notice</h2>
-              {['03', '02', '01'].map((item) => (
-                <li key={item}>
-                  <span> 공지 {item}</span>
-                  <span> 공지 {item} This is mockup contentes of notice</span>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="petsitter">
-            <button>Petsitter 바로가기</button>
-          </div>
-        </div>
-    )
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/announcements");
+  };
+
+  const handlePetSitterNavigate = () => {
+    navigate("/PetSitter");
+  };
+
+  return (
+    <div className="box noticeAndPetsitter">
+      <div className="notice">
+        <ul>
+          <h2 onClick={handleNavigate}>Notice</h2>
+          {["03", "02", "01"].map((item) => (
+            <li key={item}>
+              <span> 공지 {item}</span>
+              <span> 공지 {item} This is mockup contents of notice</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="petsitter">
+        <button onClick={handlePetSitterNavigate}>Petsitter 바로가기</button>
+      </div>
+    </div>
+  );
 }
