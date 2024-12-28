5dfa5094de0b5a4b78ebb319e1b5d25ed369ff2b
kim dahye <makamong0000@gmail.com>
2024-12-28 09:43:23 +0900

    241228kdh

---
+++ project-rrs/src/components/PetSitter/Reservation/Reservation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { DateRangePicker } from 'react-date-range';
+
+export default function Reservation() {
+  
+  return <>
+  <DateRangePicker />
+  </>
+}
+
