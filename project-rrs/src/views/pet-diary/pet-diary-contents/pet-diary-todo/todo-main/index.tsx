aeae955f20ada5b64e036fecb31c44c21e30f32b
kim dahye <makamong0000@gmail.com>
2025-01-22 19:11:59 +0900

    250122kdh

--- project-rrs/src/views/pet-diary/pet-diary-contents/pet-diary-todo/todo-main/index.tsx
+++ project-rrs/src/views/pet-diary/pet-diary-contents/pet-diary-todo/todo-main/index.tsx
@@ -34,7 +34,6 @@ export default function TodoMain({ selectedDate }: PetDiaryTodoProps) {
 
     try {
       const todos = await fetchTodosByDay(selectedDate, token);
-      console.log("날짜111",todos)
       setTodos(todos);
     } catch (error) {
       console.error("Failed to fetch todo data", error);
